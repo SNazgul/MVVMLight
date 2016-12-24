412866ee7d0348bcf8197c5a63f5495232df50ac
SNazgul <s.nazgul@gmail.com>
2016-12-24 16:21:23 +0200

    Adding base impl. to get value and initiate execution of a the command

---
+++ MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace MVVMLight
+{
+    class MainViewModel : ViewModelBase
+    {
+        private string _status;
+        private Random _rand;
+
+        public MainViewModel()
+        {
+            _rand = new Random();
+            RequestStatus = new RelayCommand(() => Task.Factory.StartNew(() => { Thread.Sleep(1000); StatusChanged("Status " + _rand.Next().ToString());}));
+        }
+
+        public String Status
+        {
+            get { return _status; }
+            set
+            {
+                Set(() => Status, ref _status, value);                
+            }
+        }
+
+
+        public RelayCommand RequestStatus
+        {
+            get;
+            private set;
+        }
+
+        public void StatusChanged(String newStatus)
+        {
+           Status = newStatus;
+        }
+
+    }
+}
