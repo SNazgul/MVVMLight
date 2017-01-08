1d2746725d67b710cbe244839e36b476583f236f
SNazgul <s.nazgul@gmail.com>
2017-01-08 14:09:15 +0200

    Adding ViewModel locator throught DependecyProperty

---
+++ VideModelLocator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVMLight
+{
+    public static class VideModelLocator
+    {
+
+
+        public static bool GetAutoWireViewModel(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoWireViewModelProperty);
+        }
+
+        public static void SetAutoWireViewModel(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoWireViewModelProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for AutoWireViewModel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AutoWireViewModelProperty =
+            DependencyProperty.RegisterAttached("AutoWireViewModel", typeof(bool), typeof(VideModelLocator), new PropertyMetadata(false, AutoWireViewModelChanged));
+
+        private static void AutoWireViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (GalaSoft.MvvmLight.ViewModelBase.IsInDesignModeStatic)
+                return;
+
+            var viewType = d.GetType();
+            var viewTypeName = viewType.FullName;
+            var viewModelTypeName = viewTypeName + "Model";
+            var viewModelType = Type.GetType(viewModelTypeName);
+            if (viewModelType == null)
+            {
+                viewModelType = typeof(MainViewModel);
+            }
+            var viewModel = Activator.CreateInstance(viewModelType);
+            ((FrameworkElement)d).DataContext = viewModel;
+        }
+    }
+}
